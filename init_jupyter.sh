0cd7431ae259511c8bb47725b71f055107e9690c
TheAIWizard <37664429+TheAIWizard@users.noreply.github.com>
2022-07-13 10:29:50 +0200

    Create init_jupyter.sh

---
+++ init_jupyter.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Clone the repository in /home/jovyan/work
+REPO=tuto-interactif-minimal
+REPO_URL=https://github.com/TheAIWizard/${REPO}.git
+git clone $REPO_URL $ROOT_PROJECT_DIRECTORY/${REPO}
+chown -R jovyan:users $ROOT_PROJECT_DIRECTORY/${REPO}
+
+# Install dependencies
+[ -f /home/jovyan/work/requirements.txt ] && pip install -r /home/jovyan/work/requirements.txt
+
+# Open a given notebook when starting Jupyter Lab
+[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/${REPO}/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
